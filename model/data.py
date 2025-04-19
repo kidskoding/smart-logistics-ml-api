d8bc5915a526dedeb75c56e201cb087107bb5c0c
Anirudh Konidala <kidskodingclub@gmail.com>
2025-04-19 06:57:22 -0500

    added a connection port through python

---
+++ model/data.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_url = os.getenv("DATABASE_URL")
+if db_url is None:
+    raise ValueError("DATABASE_URL not found in .env file")
+
+engine = create_engine(db_url)
+
+tracking_info_df = pd.read_sql("SELECT * FROM tracking_info", engine)
+timestamps_df = pd.read_sql("SELECT * FROM timestamps", engine)
+
+print(tracking_info_df.head())
+print(timestamps_df.head())
